fe00af2b96f54f846c975393d08591b37d7ca5cf
Rinvernal <irina.elkina2006@gmail.com>
2024-11-19 19:35:14 +0200

    fixed errors

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,19 +2,14 @@ import { IoMdPerson } from "react-icons/io";
 import { FaPhone } from "react-icons/fa6";
 import s from "./Contact.module.css"
 
-const Contact = ({filter, handleDeleteContact}) => {
+const Contact = ({handleDeleteContact, contact}) => {
   return (
-    <div className={s.box}>
-      {filter.map(contact => (
-        <section className={s.contact} key={contact.id}>
-          <div className={s.wrapper}>
-            <h2 className={s.item}><IoMdPerson className={s.icon}/>{contact.name}</h2>
-            <h2 className={s.item}><FaPhone className={s.icon}/>{contact.number}</h2>
-          </div>
-          <button className={s.button} onClick={() => {handleDeleteContact(contact.id)}}>Delete</button>
-        </section>
-      )) }
-      
+    <div className={s.boxContact}>
+      <div className={s.wrapper}>
+        <h2 className={s.item}><IoMdPerson className={s.icon}/>{contact.name}</h2>
+        <h2 className={s.item}><FaPhone className={s.icon}/>{contact.number}</h2>
+      </div>
+      <button className={s.button} onClick={() => {handleDeleteContact(contact.id)}}>Delete</button>
     </div>
   )
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,26 @@
 import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css"
 export const ContactList = ({contacts, handleDeleteContact}) => {
   return (
-    <div>
-      <Contact filter = {contacts} handleDeleteContact = {handleDeleteContact}/>
-    </div>
+    <ul className={s.box}>
+      {contacts.map(contact => (
+        <li className={s.contact} key={contact.id}>
+          <Contact contact = {contact} handleDeleteContact = {handleDeleteContact}/>
+        </li>
+      ))}
+      
+    </ul>
   )
 }
+{/* <div className={s.box}>
+      {filter.map(contact => (
+        <section className={s.contact} key={contact.id}>
+          <ul className={s.wrapper}>
+            <li className={s.item}><IoMdPerson className={s.icon}/>{contact.name}</li>
+            <li className={s.item}><FaPhone className={s.icon}/>{contact.number}</li>
+          </ul>
+          <button className={s.button} onClick={() => {handleDeleteContact(contact.id)}}>Delete</button>
+        </section>
+      )) }
+      
+    </div> */}
