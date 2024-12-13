6978fa060b8bd7cd1e99bb59cbe6651eac6bb5da
Rinvernal <irina.elkina2006@gmail.com>
2024-12-13 08:24:37 +0200

    debug

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { contactReducer } from "./contactSlice";
+import { filterReducer } from "./filtersSlice";
 
 
 const persistConfig = {
@@ -15,6 +16,7 @@ const persistedReducer = persistReducer(persistConfig, contactReducer);
 export const store = configureStore({
   reducer: {
     contacts: persistedReducer,
+    filter: filterReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
